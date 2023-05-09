8503f92928349a89e8a163694f99bd6ea7ba4838
Bradley Nordin <bpnordin@gmail.com>
2023-05-08 22:39:42 -0600

    added a url parse

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use config::Config;
+use std::collections::HashMap;
+use url::Url;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // get the google config
+    let config = Config::builder()
+        .add_source(config::File::new("secrets",config::FileFormat::Ini))
+        .build()
+        .unwrap();
+    let api_key: String = config.get_string("youtube.api_key")?;
+    let client = reqwest::blocking::Client::new();
+    let base_url: &str = "https://www.googleapis.com/youtube/v3/commentThreads";
+
+    let video_url: &str = "https://youtu.be/Ou5xmqgkN9c";
+
+    // parse the url for  the video is
+    let url = Url::parse(video_url).unwrap();
+
+    let video_id = url.path().split('/').last().unwrap();
+
+    // add paramaters 
+    let mut params = HashMap::new();
+    params.insert("part","snippet,replies");
+    params.insert("videoId", video_url);
+    params.insert("key",&api_key);
+    
+    let resp_1 = client.get(base_url)
+        .query(&params)
+        .send()?
+        .text()?;
+    println!("{:#?}", resp_1);
+    
+    println!("Video ID: {}", video_id);
+    Ok(())
+}
+
+fn parse_youtube_url(video_url: &str) -> Option<String> {
+    //parse the url and get the video id from the url
+    //make sure the url is from youtube
+    //the second is just a paramater of v
+
+    let url = match Url::parse(video_url){
+        Ok(url) => url,
+        Err(err) =>{
+            eprintln!("Error: {}", err);
+            return None
+        }
+    };
+
+    //there are 2 types of url, the shared youtu.be/VIDEO_ID
+    //and the youtube.com/watch?v=Z7DZs4OxOls
+    let video_id = match url.host_str()
+    {
+        Some("youtube.com") =>  url.query().map(|q| q.to_owned()),
+        Some("youtu.be") =>  url.path().split('/').last().map(|s| s.to_owned()),
+        _ =>  None
+    };
+    return video_id
+   
+
+
 }
